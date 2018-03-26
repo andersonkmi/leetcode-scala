0ad964b87a2c70b405fd157b785f629b4f014604
Anderson Ito <andersonkmi@acm.org>
2018-03-26 20:45:16 -0300

    refactor code

--- src/main/scala/org/sharpsw/leetcode/LengthLastWord.scala
+++ src/main/scala/org/sharpsw/leetcode/LengthLastWord.scala
@@ -3,6 +3,6 @@ package org.sharpsw.leetcode
 object LengthLastWord {
   def lengthOfLastWord(s: String): Int = {
     val words = s.split("\\s+")
-    if (words.length > 0) words.last.length else 0
+    if (words.nonEmpty) words.last.length else 0
   }
 }
