1f9f890b9f4a4604df2cc7c0d72d497b98f1a4f0
Anderson Ito <andersonkmi@acm.org>
2018-03-25 21:05:05 -0300

    add Length last word exercise

--- src/main/scala/org/sharpsw/leetcode/LengthLastWord.scala
+++ src/main/scala/org/sharpsw/leetcode/LengthLastWord.scala
@@ -2,6 +2,7 @@ package org.sharpsw.leetcode
 
 object LengthLastWord {
   def lengthOfLastWord(s: String): Int = {
-    0
+    val words = s.split("\\s+")
+    if (words.length > 0) words.last.length else 0
   }
 }
--- src/test/scala/org/sharpsw/leetcode/LengthLastWordSpec.scala
+++ src/test/scala/org/sharpsw/leetcode/LengthLastWordSpec.scala
@@ -1,9 +1,29 @@
 package org.sharpsw.leetcode
 
-import org.scalatest._
+import org.scalatest.{FlatSpec, Matchers}
 
 class LengthLastWordSpec extends FlatSpec with Matchers {
   "The LengthLastWord" should "return 0" in {
     LengthLastWord.lengthOfLastWord("") shouldEqual 0
+  }
+
+  "The LengthLastWord" should "return 3" in {
+    LengthLastWord.lengthOfLastWord("car") shouldEqual 3
+  }
+
+  "The LengthLastWord" should "return 3 using preceding spaces" in {
+    LengthLastWord.lengthOfLastWord("    car") shouldEqual 3
+  }
+
+  "The LengthLastWord" should "return 3 with trailing spaces" in {
+    LengthLastWord.lengthOfLastWord("car    ") shouldEqual 3
+  }
+
+  "The LengthLastWord" should "return 4" in {
+    LengthLastWord.lengthOfLastWord("car test") shouldEqual 4
+  }
+
+  "The LengthLastWord" should "return 4 with trailing spaces" in {
+    LengthLastWord.lengthOfLastWord("car test     ") shouldEqual 4
   }
 }
