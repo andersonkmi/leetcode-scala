9ef35d16de9d252d5c34b87cd98485f8760565a7
Anderson Ito <andersonkmi@acm.org>
2018-03-31 10:00:25 -0300

    add valid palindrome exercise

--- src/main/scala/org/sharpsw/leetcode/ValidPalindrome.scala
+++ src/main/scala/org/sharpsw/leetcode/ValidPalindrome.scala
@@ -2,6 +2,22 @@ package org.sharpsw.leetcode
 
 object ValidPalindrome {
   def isPalindrome(s: String): Boolean = {
-    true
+    if(s == null || s.isEmpty) true
+    else {
+      val modifiedString = keepCharsOnly(changeToLowercase(s))
+      val reversedString = modifiedString.reverse
+      modifiedString.equals(reversedString)
+    }
+  }
+
+  private def changeToLowercase(sentence: String): String = {
+    sentence.toLowerCase
+  }
+
+  private def keepCharsOnly(sentence: String): String = {
+    val ordinary=(('a' to 'z') ++ ('0' to '9')).toSet
+    sentence.map(item => {
+      if(ordinary.contains(item)) item else ""
+    }).mkString
   }
 }
