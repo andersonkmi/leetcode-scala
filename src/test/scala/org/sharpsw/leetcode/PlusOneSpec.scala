5b83f08a58ab5093cc0fdebd0ca9c9b8bcca6311
Anderson Ito <andersonkmi@acm.org>
2018-05-20 10:10:35 -0300

    add Plus one exercise

---
+++ src/test/scala/org/sharpsw/leetcode/PlusOneSpec.scala
@@ -0,0 +1,7 @@
+package org.sharpsw.leetcode
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class PlusOneSpec extends FlatSpec with Matchers {
+  //"Array [4, 3, 2, 1]" should "return [4, 3, 2, 1]"
+}
