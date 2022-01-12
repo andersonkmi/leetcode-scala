153b5fc58d41d0faecc3600dcd651144e4ca9dea
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2022-01-12 20:42:54 +0100

    more code

--- src/main/scala/org/codecraftlabs/leetcode/ContainsDuplicate.scala
+++ src/main/scala/org/codecraftlabs/leetcode/ContainsDuplicate.scala
@@ -6,7 +6,7 @@ object ContainsDuplicate {
     numbers match {
       case None => false
       case _ =>  val items = numbers.get
-                  if (items.isEmpty) false else items.groupBy(identity).mapValues(_.length).exists(item => item._2 > 1)
+                  if (items.isEmpty) false else items.groupBy(identity).view.mapValues(_.length).exists(item => item._2 > 1)
     }
   }
 }
